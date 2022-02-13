e389e0e8cbb46558aaa92337a62eac62c3dda915
bellageuse <85023957+bellageuse@users.noreply.github.com>
2022-02-13 16:03:36 +0630

    nav_and_sass

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,16 @@
 import React from "react";
-
+import "../styles/Navbar.scss";
 const Navbar = () => {
   return (
     <div>
-      <h1>this is nav bar soon to be</h1>
+      <nav className="Navbar flex">
+        <li>
+          <a href="#">About Me</a>
+          <a href="#">Projects</a>
+          <a href="#">Contacts</a>
+          <a href="#">Resume</a>
+        </li>
+      </nav>
     </div>
   );
 };
