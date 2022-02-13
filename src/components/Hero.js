10ea1bdb7008aa9ac1bc84ee349a5ac434ef0871
bellageuse <85023957+bellageuse@users.noreply.github.com>
2022-02-14 01:24:39 +0630

    hero_section

---
+++ src/components/Hero.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "../styles/Hero.scss";
+const Hero = () => {
+  return (
+    <>
+      <div className="container">
+        <h1>
+          Greetings! My name is <span>Heinsernberg.</span>
+        </h1>
+        <h3>I built dynamic responsive websites.</h3>
+        <p>
+          When I was in my sophomore year, I had to take Java and C++
+          fundamentals in my CS-minor Class. That was the first time I got to
+          know the art of programming.Even though it was so basic, I had a clear
+          realization of basic data structures and some algorithms. CS being a
+          minor subject, the journey didn't go too far.
+        </p>
+        <p>
+          Since last year , I dived into web-development.It was slow but
+          effective. From basic HTML,Css fundamentals , I kept learning till I
+          got some real-life projects.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
