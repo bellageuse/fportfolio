c3a1f1cd6c7f7decffc229ffff653d1319e1e2a3
bellageuse <85023957+bellageuse@users.noreply.github.com>
2022-02-14 14:30:35 +0630

    see_more_btn_unsolved

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Hero.scss";
+import Data from "../data/Data";
 const Hero = () => {
+  const [seeMore, setSeeMore] = useState(false);
+  const { text } = Data;
   return (
     <>
       <div className="container">
@@ -9,16 +12,10 @@ const Hero = () => {
         </h1>
         <h3>I built dynamic responsive websites.</h3>
         <p>
-          When I was in my sophomore year, I had to take Java and C++
-          fundamentals in my CS-minor Class. That was the first time I got to
-          know the art of programming.Even though it was so basic, I had a clear
-          realization of basic data structures and some algorithms. CS being a
-          minor subject, the journey didn't go too far.
-        </p>
-        <p>
-          Since last year , I dived into web-development.It was slow but
-          effective. From basic HTML,Css fundamentals , I kept learning till I
-          got some real-life projects.
+          {seeMore ? text : `${text.subString(0, 100)}`}
+          <button className="btn" onClick={() => setSeeMore(!seeMore)}>
+            {seeMore ? "See Less" : "See More"}
+          </button>
         </p>
       </div>
     </>
---
+++ src/data/Data.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Data() {
+  return (
+    <div>
+      When I was in my sophomore year, I had to take Java and C++ fundamentals
+      in my CS-minor Class. That was the first time I got to know the art of
+      programming.Even though it was so basic, I had a clear realization of
+      basic data structures and some algorithms. CS being a minor subject, the
+      journey didn't go too far. Since last year , I dived into
+      web-development.It was slow but effective. From basic HTML,Css
+      fundamentals , I kept learning till I got some real-life projects.
+    </div>
+  );
+}
+
+export default Data;
+console.log(Data);
